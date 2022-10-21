7ed463570a44c31e2e233e70d72793b29984e37a
tondeboer92 <100686344+tondeboer92@users.noreply.github.com>
2022-10-21 14:56:39 +0200

    main edits

--- MA4/MA4_Files/MA4_2.py
+++ MA4/MA4_Files/MA4_2.py
@@ -23,11 +23,11 @@ def fib_py(n):
 
 
 def main():
-    print(fib_py(6))
-#	f = Person(5)
-#	print(f.get())
-#	f.set(7)
-#	print(f.get())
+	print(fib_py(6))
+f = Person(5)
+print(f.get())
+f.set(7)
+print(f.get())
 
 if __name__ == '__main__':
 	main()
