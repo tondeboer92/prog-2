d6af83b1bb6d64983f7719f5e663c90de282da9f
tondeboer92 <100686344+tondeboer92@users.noreply.github.com>
2022-10-22 03:15:16 +0200

    Update MA4_2

--- MA4/MA4_Files/MA4_2.py
+++ MA4/MA4_Files/MA4_2.py
@@ -2,7 +2,7 @@
 
 from person import Person
 from numba import njit
-
+from time import perf_counter as pc
 
 
 @njit
@@ -23,20 +23,22 @@ def fib_py(n):
 
 
 def main():
-    
 	f = Person(5)
 	start1 = pc()
 	print(f.get())
 	fib_py(f.get())
 	end1 = pc()
+	print(f"Process took {round(end1-start1, 2)} seconds")
 	start2 = pc()
 	print("numba")
 	fib_numba(f.get())
 	end2 = pc()
-	start1 = pc()
+	print(f"Process took {round(end2-start2, 2)} seconds")
+	start3 = pc()
 	print("person")
 	f.fib()
-	end1 = pc()
+	end3 = pc()
+	print(f"Process took {round(end3-start3, 2)} seconds")
 
 if __name__ == '__main__':
 	main()
