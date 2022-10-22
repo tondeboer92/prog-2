bf7535ab90fdae05a200176c6bc3fa3382e1c054
tondeboer92 <100686344+tondeboer92@users.noreply.github.com>
2022-10-22 19:16:48 +0200

    syncing person.cpp

--- MA4/MA4_Files/person.cpp
+++ MA4/MA4_Files/person.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib> 
+// Person class
 
-// Person class 
+
+
 
 class Person{
 	public:
@@ -10,6 +12,7 @@ class Person{
 		int fib();
 	private:
 		int age;
+		int fib_res(int);
 	};
  
 Person::Person(int n){
@@ -17,11 +20,14 @@ Person::Person(int n){
 	}
 
 int Person::fib(){
-	if (get() <= 1)
-		return (get());
-	else {
-		return Person(get()-1).fib() + Person(get()-2).fib();
+		return fib_res(self.age);
 }
+
+int Person::fib_res(int a){
+	if (age <= 1)
+		return (age);
+	else 
+		return fib_res(a-1) + fib_res(a-2);
 }
  
 int Person::get(){
@@ -37,11 +43,12 @@ extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
 	void Person_set(Person* person, int n) {person->set(n);}
-	int Person_fib(Person* person){return person->fib();}
+	int Person_fib(Person* person) {return person->fib();}
 	void Person_delete(Person* person){
 		if (person){
 			delete person;
 			person = nullptr;
 			}
 		}
-	}
+	}	
+	
