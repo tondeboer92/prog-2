d4be727e43e2866256ed69a6abc737a9409d8710
Ton de Boer <tode1131@arrhenius.it.uu.se>
2022-10-23 15:52:23 +0200

    working diagrams

--- MA4/MA4_Files/MA4_2.py
+++ MA4/MA4_Files/MA4_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-# #include <cstdlib>
- 
+
+#include <cstdlib>
+
 from person import Person
 from numba import njit
 from time import perf_counter as pc
@@ -28,12 +29,18 @@ def fib_py(n):
 def main():
     x = []
     y = []
-    for n in range(30, 46):
+    for n in range(35,39):
         start = pc()
         fib_py(n)
         end = pc()
         print(f"Process took {round(end-start)} seconds")
         x.append(n)
-        y.append({round(end-start)})
-    diag1 = plt.plot(x, y)
-    diag1.savefig("fib_py n = 30-45")
+        y.append(round(end-start))
+    print(x)
+    print(y)
+    plt.plot(x,y)
+    plt.savefig("Fig1")
+
+if __name__ == '__main__':
+	main()
+
